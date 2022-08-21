8ff48dc086538696411ce3102327f0867da61f87
abyssofbirds <deniskind@inbox.ru>
2022-08-21 20:04:20 +0300

    feat: css, content

--- index.js
+++ index.js
@@ -4,15 +4,24 @@ document.addEventListener("DOMContentLoaded", function () {
   const header = document.getElementsByClassName("header")[0];
 
   let isHeaderExpanded = true;
+  let counter = 0;
   outerWrapper.addEventListener("scroll", (event) => {
-    console.log(event.target.scrollTop);
+    // console.log(event.target.scrollTop);
     if (event.target.scrollTop > 50 && isHeaderExpanded) {
       isHeaderExpanded = false;
+      counter++;
+      console.log(counter);
       header.classList.toggle("expanded-header", false);
     }
     if (event.target.scrollTop < 50 && !isHeaderExpanded) {
       isHeaderExpanded = true;
+      counter++;
+      console.log(counter);
       header.classList.toggle("expanded-header", true);
+    }
+    if (counter === 8) {
+      alert("Please, stop it");
+      counter = 0;
     }
   });
 });
